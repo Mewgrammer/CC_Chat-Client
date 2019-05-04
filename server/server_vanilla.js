b3ffe4561b44b2eb2308bf0c5debc71406a315fd
Michael.Mew <michael4mew@gmail.com>
2019-05-04 23:06:37 +0200

    server now written in typescript

--- server/server_vanilla.js
+++ server/server_vanilla.js
@@ -10,26 +10,19 @@ const fs = require("fs");
 const cors = require("cors");
 
 
-const app = express();
-const port = process.env.PORT || 8080;
-const httpsPort = process.env.HTTPS_PORT || 8443;
-const corsOptions = {
-  origin: (origin, callback) => {
-      callback(null, true);
-  },
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-const privateKey = fs.readFileSync(path.join(__dirname, "ssl", "privatekey.pem").toString());
-const certificate = fs.readFileSync(path.join(__dirname, "ssl", "certificate.pem").toString());
-const secureOptions = {
-  key: privateKey,
-  cert: certificate,
-  requestCert: false,
-  rejectUnauthorized: false,
-};
+
+
 
 try {
+  const app = express();
+  const port = process.env.PORT || 8081;
+  const corsOptions = {
+    origin: (origin, callback) => {
+        callback(null, true);
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+  };
   app.use(cors(corsOptions));
   app.use(express.static(path.join(__dirname)));
   app.use(helmet());
@@ -38,16 +31,28 @@ try {
     maxAge: 15552000  // 180 days in seconds
   }));
   app.get("/*", (req, res) => {
+    console.log("get");
     res.sendFile(path.join(__dirname));
   });
 
   const server = http.createServer(app);
   server.listen(port, () => console.log("HTTP-Server listening on Port %s", port));
 
+  /*
+  const httpsPort = process.env.HTTPS_PORT || 8443;
+  const privateKey = fs.readFileSync(path.join(__dirname, "ssl", "privatekey.pem").toString());
+  const certificate = fs.readFileSync(path.join(__dirname, "ssl", "certificate.pem").toString());
+  const secureOptions = {
+    key: privateKey,
+    cert: certificate,
+    requestCert: false,
+    rejectUnauthorized: false,
+  };
   const secureServer = https.createServer(secureOptions, app);
   secureServer.listen(httpsPort, () => {
     console.log("HTTPS-Server listening on Port %s", httpsPort)
   });
+  */
 
 }
 catch(err) {
